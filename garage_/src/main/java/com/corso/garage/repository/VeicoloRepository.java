ee3e041d0d9bda47a19cdfd1ad8bb2cda9977db5
Emiliano <ilfratellomigliore@gmail.com>
2026-06-04 17:28:41 +0200

    Delete garage_ directory

--- garage_/src/main/java/com/corso/garage/repository/VeicoloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.corso.garage.repository;
-
-import com.corso.garage.entity.Veicolo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VeicoloRepository extends JpaRepository<Veicolo, Long> {
-}
